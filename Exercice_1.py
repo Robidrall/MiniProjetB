3669bdd32474ea42915fc8a5aca4f91b2be2ebfd
P3PiT0 <lucas.chz@laposte.net>
2023-05-31 15:04:24 -0400

    Fin codage python basique

--- Exercice_1.py
+++ Exercice_1.py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import timeit
+from timeit import timeit
 import time
 
 def integration_exacte (a,b,p1,p2,p3,p4): 
     return p1*(b-a)+p2*(b**2-a**2)/2 +p3*(b**3-a**3)/3 + p4*(b**4-a**4)/4
-    
-    
+       
 def det_solution_analytique(x,p1,p2,p3,p4):
     return p1 + p2*x +p3*x**2 + p4*x**3
 
@@ -16,27 +15,32 @@ def methode_des_rectangles(n,p1,p2,p3,p4):
    b = 3
    pas = (b - a) / n
    intervalle = [a + i * pas for i in range(n + 1)]
-   print(intervalle)
    for i in range(n):
         aire = det_solution_analytique( intervalle[i]+ (intervalle[i + 1] - intervalle[i])/2,p1,p2,p3,p4) * (intervalle[i + 1] - intervalle[i])
         somme += aire
    return somme
 
+def erreur_integration_num (valeur_exacte, valeur_calcule):
+    erreur= valeur_exacte-valeur_calcule
+    return erreur
+
+def Convergence_selon_n (n,p1,p2,p3,p4):
+    return round(100*erreur_integration_num(integration_exacte (-2,3,p1,p2,p3,p4),methode_des_rectangles(n,p1,p2,p3,p4))/integration_exacte (-2,3,p1,p2,p3,p4),5)
 
 p1=5
 p2=2
 p3=4
 p4=2
-somme = methode_des_rectangles(10,p1,p2,p3,p4)
-print(somme)
 
-somme = methode_des_rectangles(100,p1,p2,p3,p4)
+print(Convergence_selon_n(15,p1,p2,p3,p4))
+print(Convergence_selon_n(50,p1,p2,p3,p4))
+print(Convergence_selon_n(500,p1,p2,p3,p4))
+print(timeit('Convergence_selon_n(500,p1,p2,p3,p4)',globals=globals(),number=1))
 
-print(somme)
 
 
-valeur_reelle = integration_exacte(-2,3,5,2,4,2)
-print(valeur_reelle)
+
+
 
 
 
